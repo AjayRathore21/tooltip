ce4020976bcadeface6bb284b12f80462018ee27
Ajay Rathore <ajaykumar420.ak79@gmail.com>
2023-05-11 00:42:43 +0530

    project setted up!

---
+++ src/Tooltip.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Tooltip.css"
+
+const Tooltip = () => {
+    return (
+        <div>
+            <h1>Hover on me....</h1>
+            
+        </div>
+    );
+};
+
+export default Tooltip;
