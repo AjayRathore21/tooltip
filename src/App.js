294eb518552fc5885c660f0e16c47261dd7e9e13
Ajay Rathore <ajaykumar420.ak79@gmail.com>
2023-05-11 11:20:17 +0530

    tooltip is working and directions is passed via props!

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 import Tooltip from './Tooltip';
 
 function App() {
+
+ const tooltipPosition = {
+    top:"top",
+    bottom:"bottom",
+    left:'left',
+    right:'right'
+  }
+
+
+
   return (
     <div className="App">
-      <Tooltip></Tooltip>
+      <Tooltip position = {tooltipPosition}></Tooltip>
       
     </div>
   );
--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import "./Tooltip.css"
+import React, { useState } from "react";
+import "./Tooltip.css";
 
-const Tooltip = () => {
-    return (
-        <div>
-            <h1>Hover on me....</h1>
-            
-        </div>
-    );
+const Tooltip = (props) => {
+  const [active, setActive] = useState(false);
+
+  const showTooltip = () => {
+    setActive(true);
+  };
+
+  const hideTooltip = () => {
+    setActive(false);
+  };
+
+//   console.log(props.position.top);
+
+  return (
+    <div>
+      <div
+        onMouseEnter={showTooltip}
+        onMouseLeave={hideTooltip}
+        class="Tooltip-Wrapper"
+      >
+        Hover over me
+        {active ? (
+          <span className={`Tooltip-Tip ${props.position.left}`}>
+            Tooltip text
+          </span>
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default Tooltip;
